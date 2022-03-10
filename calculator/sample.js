271a14eeeedd0f71c0f4700f8aae300ededf6b42
MadhuAloor <madhu.aloor@gmail.com>
2022-03-10 23:07:39 +0530

    coding with javascript

---
+++ calculator/sample.js
@@ -0,0 +1,206 @@
+// // // add two numbers
+// // const num1=10;
+// // const num2=10;
+// // const sum=num1+num2;
+// // console.log("sum of" +num1+ 'and'+num2+ "is" +sum);
+
+
+// // //find square root
+// //  const jj =Math.sqrt("hello");
+// //  console.log(jj);
+
+// //  //area of triangle
+// //  const area =(4*6)/2;
+// //  console.log(area);
+
+// //  //swapuing two variables
+
+// //  let a = 4;
+// //  let b = 3;
+// //  let temp;
+// //  temp=a;
+// //  a=b;
+// //  b=temp;
+// //  console.log(a);
+// //  console.log(b);
+
+// //  //kilometer to miles
+
+// // //  const kilometer=prompt("enter the kilometer:");
+ 
+// // //  const factor=0.621371;
+// // //  const miles=(kilometer*factor);
+// // //  console.log("${miles} miles:");
+ 
+// //  //celsious to fahrenhiet
+// //  var celsious=console.log("enter the celsious value");
+// // var celsious=50;
+// // console.log(celsious);
+// //  const fahernheit =(celsious*1.8)+32
+
+// //  console.log(fahernheit);
+
+// //random num
+//   const ab =Math.random(20);
+//  console.log(ab);
+
+// //check the number
+// // var anke = console.log("enter the number");
+// // var anke = -1;
+// // console.log(anke);
+// // if (anke>0){
+// //     console.log("number is possitive");
+
+// // }else if(anke==0){
+// //     console.log("number is zero");
+// // }
+// // else {
+// //     console.log("number is negetive")
+// // }
+
+// //even or odd
+
+// const eve =21;
+// if(eve%2==0){
+//     console.log("even");
+// }else if(eve%2==1){
+//     console.log("odd");
+// }
+//  //largest number
+
+//  const num1 = 22;
+//  const num2 = 29;
+//  const num3 = 28;
+
+//  if(num1>=num2 && num1>=num3){
+//      console.log("number 1 great ");
+//  }
+//  else if(num2>=num1 && num2>=num3){
+//      console.log("number 2 greater ");
+//  }
+//  else{
+//      console.log("number 3 greater");
+//  }
+
+//  //prime numbers
+//  const x =2;
+//  if(x%2!=0){
+//      console.log("prime number");
+//  }
+//  else {
+//      console.log("not a prime number");
+//  }
+// //prime number
+//  (number === 1) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+
+// // check if number is greater than 1
+// else if (number > 1) {
+
+//     // looping through 2 to number-1
+//     for (let i = 2; i < number; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+
+//     if (isPrime) {
+//         console.log(`${number} is a prime number`);
+//     } else {
+//         console.log(`${number} is a not prime number`);
+//     }
+// }
+
+// // check if number is less than 1
+// else {
+//     console.log("The number is not a prime number.");
+// }
+// // prime number
+
+// // program to check if a number is prime or not
+
+// // take input from the user
+// const number = parseInt(prompt("Enter a positive number: "));
+// let isPrime = true;
+
+// // check if number is equal to 1
+// if (number === 1) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+
+// // check if number is greater than 1
+// else if (number > 1) {
+
+//     // looping through 2 to number-1
+//     for (let i = 2; i < number; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+// }
+
+//     if (isPrime) {
+//         console.log(` is a prime number`);
+//     } else {
+//         console.log('is a not prime number`);
+//     }
+
+
+// // check if number is less than 1
+// else {
+//     console.log("The number is not a prime number.");
+// }
+
+
+//calculator
+
+// const operator =prompt("enter the oerator");
+// const number1= 20;
+// const number2 = 30;
+// let result;
+// if(operator=='+'){
+//     result =number1+number2;
+// }
+// else if(operator=='-'){
+//     result=number1-number2;
+
+// }
+// else if(operator=='*'){
+//     result=number1*number2;
+// }
+// else{
+//     result=number1/number2;
+// }
+
+// console.log(result);
+
+
+//sum of natural number
+// const row =9;
+// let sum =0;
+// for(let i= 1;i<=row;i++) {
+//     sum +=i;
+// }
+// console.log(sum);
+
+//multiplication table
+
+const number =3;
+for(let i=1;i<=10;i++){
+    const result = number *i;
+
+console.log(result);
+}
+
+
+//date to number
+const d1=new Date().getTime();
+console.log(d1);
+
+
+
+
+
